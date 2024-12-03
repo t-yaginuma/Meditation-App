e069880b3c490f5188dd84fdcc1d1dfd5ba95b33
t-yaginuma <toru45s@gmail.com>
2024-12-03 16:30:45 +0900

    add first page

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,20 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
+import React from "react";
+import beachImage from "@/assets/meditation-images/beach.webp";
 import "../global.css";
 
-export default function HomeScreen() {
+const index = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
+    <View className="flex-1 h-full">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1 h-full"
+      >
+        <Text>index</Text>
+      </ImageBackground>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+export default index;
